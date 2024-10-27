353d3b9a6d71dc1769bcdc988014aabd25d54744
Michael Campos <mjcampos@users.noreply.github.com>
2024-10-27 19:43:32 +0100

    Setup Player
    Also adjusted ball speed

--- scenes/ball/ball.gd
+++ scenes/ball/ball.gd
@@ -1,12 +1,12 @@
 extends CharacterBody2D
 
-var speed = 500.0
+var speed = 600.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var random_angle = randf() * 360
 	
-	velocity = Vector2(cos(random_angle), sin(random_angle)) * 500
+	velocity = Vector2(cos(random_angle), sin(random_angle)) * speed
 
 func _physics_process(delta):
 	var collision: KinematicCollision2D = move_and_collide(velocity * delta)
---
+++ scenes/player/player.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+
+var speed: float = 400.0
+var fixed_x_position: float
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	fixed_x_position = position.x
+
+
+func _physics_process(delta):
+	var direction = Input.get_axis("ui_up", "ui_down")
+	
+	velocity.y = direction * speed
+	
+	move_and_slide()
+
+	position.x = fixed_x_position
