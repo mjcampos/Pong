8f13dfdeed83dc1dc8aa8ca3670006c6846eaa9c
Michael Campos <mjcampos@users.noreply.github.com>
2024-10-28 10:16:21 +0100

    Set countdown labels

--- scenes/game/game.gd
+++ scenes/game/game.gd
@@ -1,14 +1,20 @@
 extends Node2D
 
 @onready var timer: Timer = $Timer
+@onready var number: Label = $CanvasLayer/Number
 
 var countdown: int = 3
+
+func _ready():
+	number.text = str(countdown)
+	number.visible = true
 
 func _on_timer_timeout():
 	countdown -= 1
 	
 	if countdown > 0:
-		print(str(countdown) + " seconds left")
+		number.text = str(countdown)
 		timer.start()
 	else:
+		number.visible = false
 		SignalManager.on_countdown_complete.emit()
