bdb18fb3b837c129375c161e088baea72fe66f24
Michael Campos <mjcampos@users.noreply.github.com>
2024-10-28 17:50:13 +0100

    Fixed background music

--- singletons/background_music.gd
+++ singletons/background_music.gd
@@ -1,6 +1,9 @@
-extends AudioStreamPlayer2D
+extends AudioStreamPlayer
 
+const BACKGROUND_MUSIC = preload("res://assets/background_music.mp3")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	stream = BACKGROUND_MUSIC
+	volume_db = -15
 	play()
